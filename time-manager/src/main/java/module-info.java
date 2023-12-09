e83d5fd913306c433252c3d752ae6e0e37323382
Martin Mitrov <136697473+martiindmv@users.noreply.github.com>
2023-12-09 19:48:17 +0100

    Add files via upload
    Initial

---
+++ time-manager/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.timemanager {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.timemanager to javafx.fxml;
+    exports com.example.timemanager;
+}
