63d1d07e154ecfb57a69c4c0d2b9898d8e8d6b86
martiindmv <martin.mitrov2@gmail.com>
2023-12-10 01:26:04 +0100

    Updating the pom.xml to include javafx

--- time-manager/src/main/java/module-info.java
+++ time-manager/src/main/java/module-info.java
@@ -4,11 +4,6 @@ module com.example.timemanager {
     requires javafx.web;
 
     requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
 
     opens com.example.timemanager to javafx.fxml;
     exports com.example.timemanager;
